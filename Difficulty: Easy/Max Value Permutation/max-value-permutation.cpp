11e6539f41071bfa18c409cadc12ec76e61bae31
2004vivek <119566938+2004vivek@users.noreply.github.com>
2024-08-23 23:54:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max Value Permutation/max-value-permutation.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int Maximize(vector<int> &arr) {
+        // Complete the function
+        long int mod=1e9+7;
+        long s=0;
+        sort(arr.begin(),arr.end());
+        for(long long i=0;i<arr.size();i++){
+            s=(s+arr[i]*i)%mod;
+        }
+        return s;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        //  cin.ignore();
+        int n = arr.size();
+        Solution ob;
+        cout << ob.Maximize(arr) << endl;
+    }
+}
+// } Driver Code Ends
