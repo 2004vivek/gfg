b8b4dbeb9db644f78d7adb4401caeceae2b91e31
2004vivek <119566938+2004vivek@users.noreply.github.com>
2024-12-15 22:16:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.cpp
+++ Difficulty: Basic/Peak element/peak-element.cpp
@@ -9,29 +9,21 @@ class Solution {
   public:
     int peakElement(vector<int> &arr) {
         // Your code here
-        int id=0;
+        int c=0;
         int n=arr.size();
-       if(n==1){
-           return id;
-       }
-       for(int i=0;i<n;i++){
-           if(i==0){
-               if(arr[i]>=arr[i+1]){
-                   return i;
-               }
-           }
-           else if(i==n-1){
-               if(arr[i]>=arr[i-1]){
-                    return i;
-               }
-               }
-           else {
-               if(arr[i]>=arr[i+1]&&arr[i]>=arr[i-1]){
-                   return i; 
-               }
-           }
-       }
-       return id;
+        if(n==1){
+            return 0;
+        }
+        for(int i=1;i<arr.size()-1;i++){
+            if(arr[i]>arr[i-1]&&arr[i]>arr[i+1]){
+                c=1;
+                return i;
+            }
+        }
+        if(arr[n-1]>arr[n-2]){
+            return n-1;
+        }
+        
     }
 };
 
