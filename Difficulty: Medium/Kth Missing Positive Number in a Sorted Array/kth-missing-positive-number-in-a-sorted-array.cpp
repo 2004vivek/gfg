fc8d77968df5632c9a4712b018aa4cf52a8eea89
2004vivek <119566938+2004vivek@users.noreply.github.com>
2024-12-19 14:04:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int kthMissing(vector<int> &arr, int k) {
+        // Your code goes here
+        vector<int>v(10e5,0);
+        int n=arr.size();
+        
+        
+       for(int i=0;i<n;i++){
+           v[arr[i]]=1;
+       }
+       int c=0;
+       int x;
+    //   for(int i=0;i<10e5;i++){
+    //       cout<<v[i]<<" ";
+    //   }
+       for(int i=1;i<=10e5;i++){
+        //   cout<<v[i]<<" ";
+           if(v[i]==0){
+               c++;
+           }
+           if(c==k){
+               x=i;
+               break;
+           }
+        //   if(v[i]==0&&c<k){
+        //       i++;
+        //       c++;
+        //   }
+        //   if(c==k){
+        //       x=i;
+        //       break;
+        //   }
+       }
+       return x;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.kthMissing(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
