a1767b89437d90afa8f750a01113d88c34a91153
2004vivek <119566938+2004vivek@users.noreply.github.com>
2024-12-28 23:11:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find All Triplets with Zero Sum/find-all-triplets-with-zero-sum.cpp
+++ Difficulty: Medium/Find All Triplets with Zero Sum/find-all-triplets-with-zero-sum.cpp
@@ -13,9 +13,9 @@ class Solution {
         for(int i=0;i<arr.size();i++){
             for(int j=i+1;j<arr.size();j++){
                 for(int k=j+1;k<arr.size();k++){
-                    if(arr[i]+arr[j]+arr[k]==0){
-                        v.push_back({i,j,k});
-                    }
+                  if(arr[i]+arr[j]+arr[k]==0){
+                      v.push_back({i,j,k});
+                  }
                 }
             }
         }
