9605ebd7975f05e8fbb13ce2ae43e4f338088a35
Vivek Kumar <119566938+2004vivek@users.noreply.github.com>
2025-06-13 16:29:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
+++ Difficulty: Medium/Koko Eating Bananas/koko-eating-bananas.cpp
@@ -1,60 +1,32 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
+    int bananas(int mid,vector<int>&arr){
+        int time=0;
+        for(int i=0;i<arr.size();i++){
+            time+=ceil((double)arr[i]/mid);
+        }
+        return time;
+    }
     int kokoEat(vector<int>& arr, int k) {
         // Code here
+        int mini=1;
         int maxi=*max_element(arr.begin(),arr.end());
-        int i=1,j=maxi;
-        int s=0;
+        int mid;
         int ans=-1;
-        while(i<=j){
-            s=0;
-            int mid=(i+j)/2;
-            for(int k=0;k<arr.size();k++){
-                s+=(ceil)((double)arr[k]/mid);
-            }
-            if(s<=k){
+        while(mini<=maxi){
+             mid=(mini+maxi)/2;
+            int time=bananas(mid,arr);
+            if(time<=k){
                 ans=mid;
-                j=mid-1;
+                 maxi=mid-1;
+
             }
+          
             else{
-                i=mid+1;
+                  mini=mid+1;
             }
         }
         return ans;
+     
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-
-        cout << ob.kokoEat(arr, k);
-        cout << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
